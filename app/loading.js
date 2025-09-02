270a9a579757303bce51d702bdddda5baf0bb550
Riyashukla123 <riya70077@gmail.com>
2025-09-02 16:31:51 +0530

    loading

--- app/loading.js
+++ app/loading.js
@@ -1,13 +1,12 @@
 
 import Image from "next/image"
-import { MdOutlineLocalDining } from "react-icons/md"
 const loading=()=>{
   return(
-    <div className="flex z-[10000] flex-col justify-center items-center h-screen w-full fixed bg-purple-700">
+    <div className="flex flex-col justify-center items-center h-screen w-full fixed bg-purple-700">
       <div className="object contain flex justify-center items-center">
         <Image src="/logo.png" alt="logo" width={200} height={200}/>
       </div>
     </div>
   )
 }
-export default MdOutlineLocalDining
+export default loading
